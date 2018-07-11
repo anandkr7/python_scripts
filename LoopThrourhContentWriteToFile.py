b006cf9bbcd21c25ccf07b37d0a83d820be87154
Administrator <anandkr7@gmail.com>
2018-07-11 12:17:01 +0530

    Loop through content and write to a new file script

--- LoopThrourhContentWriteToFile.py
+++ LoopThrourhContentWriteToFile.py
@@ -1,13 +1,14 @@
-btidfilepath = '/home/anand/BTID_Issues_5258'  
-sqlscriptfilepath = '/home/anand/Bug_5258_HDFC _lookup_entries_27091975.sql'
+btidfilepath = '/home/anand/ListOfEntries'  
+sqlscriptfilepath = '/home/anand/Content.txt'
 a = open(sqlscriptfilepath, 'r')
 content = a.read()
-w = open("/home/anand/Bug_5258_Scripts.sql","a+")
+w = open("/home/anand/ResultFile.sql","a+")
 with open(btidfilepath) as fp:  
 	for cnt, line in enumerate(fp):
 		w.write("\n\n")
 		w.write("#%d" % cnt + "--" + line)
-		changedContent = content.replace("27091975", line.strip())
+		changedContent = content.replace("Content", line.strip())
 		w.write(changedContent)
 		
 		
+
